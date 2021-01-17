e5e0a45fab3c199a89c5e1958d1938ad09238d84
clinton <clinton.l.anton@gmail.com>
2021-01-16 19:59:48 -0500

    commit tree 1e64cf78215c68233f49622be921b52711c67e47

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,37 +1,29 @@
-def roll_call_dwarves(dwarfs)# code an argument here
-  # Your code here
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(dwarfs)
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
 end
 
-def summon_captain_planet(veggies)# code an argument here
-  # Your code here
-  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-    veggies.collect do |call| 
-    call.capitalize + "!"
+def summon_captain_planet(array)
+  array.map do |element|
+    element[0] = element[0].upcase
+    element << "!"
   end
+end	
 
-end
-
-def long_planeteer_calls(array) 
-  i = 0 
-  if  array.any? {|i| i.length > 4}
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|i| i.length > 4}
     return true
   else 
     return false
-  i = i + 1
-  end 
+  i += 1
+  end
 end
-  
 
-
-
-def find_the_cheese(cheese)# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
-    cheese_types.include?(cheese)
+    cheese_types.find do |cheese|
+      return cheese if array.include?(cheese) == true
   end
 end
